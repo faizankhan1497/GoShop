fe18aaafafee7f4c7f9767611a43e6e8d26f226c
Faizan Khan <faizan_khan1497@yahoo.com>
2021-10-10 00:56:27 +0200

    change

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -71,9 +71,5 @@ const Rating = ({ value, text, color }) => {
 Rating.defaultProps = {
   color: '#f8e825',
 }
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
-}
+
 export default Rating
