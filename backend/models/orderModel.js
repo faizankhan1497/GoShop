529a772417daede5c8fd72be8414c4f432c19529
Faizan Khan <faizan_khan1497@yahoo.com>
2021-10-14 19:51:55 +0200

    end of 56

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -22,13 +22,12 @@ const orderSchema = mongoose.Schema(
     ],
     shippingAddress: {
       address: { type: String, required: true },
-      city: { type: Number, required: true },
+      city: { type: String, required: true },
       postalCode: { type: String, required: true },
-      country: { type: Number, required: true },
+      country: { type: String, required: true },
     },
     PaymentMethod: {
       type: String,
-      required: true,
     },
     PaymentResult: {
       id: { type: String },
