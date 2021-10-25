74d14156adf5d175fc6f6d04452a8de8a609961b
Faizan Khan <faizan_khan1497@yahoo.com>
2021-10-25 18:55:09 +0200

    latest changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ChopShop</Navbar.Brand>
+            <Navbar.Brand>GoShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
